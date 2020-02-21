102fa0b2e8853efc643d203f9ed5fcae001612d5
thakurmhn <mohan_thakoor@yahoo.co.in>
2020-02-21 14:06:03 +0530

    added group and hosts vars and groups into dy inventory

--- dinventory.py
+++ dinventory.py
@@ -42,7 +42,7 @@ def main():
    args = parse_args()
    if args.list:
        hosts = list_running_hosts()
-       json.dump({'vagrant': hosts, 'centos': hosts[:-1]}, sys.stdout)
+       json.dump({'vagrant': hosts[:], 'centos': hosts[:-1]}, sys.stdout)
    else:
        details = get_host_details(args.host)
        json.dump(details, sys.stdout)
